138920cc070aa9721ec412fcdff80b7c26ea06a7
Suden <20457922+shudnal@users.noreply.github.com>
2024-08-27 04:16:13 +0300

    Add project files.

---
+++ BiomeGate.cs
@@ -0,0 +1,207 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using ServerSync;
+using System.Collections.Generic;
+using System.Reflection;
+using System;
+using System.Linq;
+using static HitData;
+using UnityEngine;
+
+namespace BiomeGate
+{
+    [BepInPlugin(pluginID, pluginName, pluginVersion)]
+    public class BiomeGate : BaseUnityPlugin
+    {
+        const string pluginID = "shudnal.BiomeGate";
+        const string pluginName = "Biome Gate";
+        const string pluginVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(pluginID);
+
+        internal static readonly ConfigSync configSync = new ConfigSync(pluginID) { DisplayName = pluginName, CurrentVersion = pluginVersion, MinimumRequiredVersion = pluginVersion };
+
+        public static BiomeGate instance;
+
+        public static ConfigEntry<bool> modEnabled;
+        private static ConfigEntry<bool> configLocked;
+        private static ConfigEntry<bool> loggingEnabled;
+
+        private static ConfigEntry<Heightmap.Biome> gatedBiomes;
+        private static ConfigEntry<bool> adminPermitted;
+
+        internal static ConfigEntry<DamageModifier> damageReceivedModifier;
+        internal static ConfigEntry<float> damageDoneModifier;
+        internal static ConfigEntry<float> noiseModifier;
+        internal static ConfigEntry<float> sneakModifier;
+        internal static ConfigEntry<float> raiseSkillModifier;
+        internal static ConfigEntry<bool> showRepeatMessage;
+        internal static ConfigEntry<bool> showStartMessage;
+
+        private void Awake()
+        {
+            harmony.PatchAll();
+
+            instance = this;
+
+            ConfigInit();
+            _ = configSync.AddLockingConfigEntry(configLocked);
+
+            Game.isModded = true;
+        }
+
+        private void OnDestroy()
+        {
+            Config.Save();
+            harmony?.UnpatchSelf();
+        }
+
+        public static void LogInfo(object data)
+        {
+            if (loggingEnabled.Value)
+                instance.Logger.LogInfo(data);
+        }
+
+        private void ConfigInit()
+        {
+            modEnabled = config("General", "Enabled", defaultValue: true, "Enable the mod.");
+            configLocked = config("General", "Lock Configuration", defaultValue: true, "Configuration is locked and can be changed by server admins only.");
+            loggingEnabled = config("General", "Logging enabled", defaultValue: false, "Enable logging. [Not Synced with Server]", false);
+
+            gatedBiomes = config("Gating", "Biomes", defaultValue: Heightmap.Biome.None, "Biome list.");
+            adminPermitted = config("Gating", "Ignore admins", defaultValue: true, "Admins and host will not be affected");
+            damageReceivedModifier = config("Gating", "Damage received modifier", defaultValue: DamageModifier.VeryWeak, "Damage modifier for incoming damage");
+            damageDoneModifier = config("Gating", "Damage dealt modifier", defaultValue: 0f, "Damage modifier for damage done by affected players");
+            noiseModifier = config("Gating", "Noise modifier", defaultValue: 10f, "Noise modifier. How far away player will be heared.");
+            sneakModifier = config("Gating", "Sneak modifier", defaultValue: -1f, "Sneak efficacy modifier");
+            raiseSkillModifier = config("Gating", "Raise skill modifier", defaultValue: -100f, "Modifier for skill raise. 0 is normal skill raise. Other status effects can also modify skill raise.");
+            showRepeatMessage = config("Gating", "Show repeat message", defaultValue: true, "Show repeated message every 20 seconds");
+            showStartMessage = config("Gating", "Show start message", defaultValue: true, "Show message when debuff is applied");
+
+            damageReceivedModifier.SettingChanged += (sender, args) => SE_BiomeGate.UpdateBiomeGateProperties();
+            damageDoneModifier.SettingChanged += (sender, args) => SE_BiomeGate.UpdateBiomeGateProperties();
+            noiseModifier.SettingChanged += (sender, args) => SE_BiomeGate.UpdateBiomeGateProperties();
+            sneakModifier.SettingChanged += (sender, args) => SE_BiomeGate.UpdateBiomeGateProperties();
+            raiseSkillModifier.SettingChanged += (sender, args) => SE_BiomeGate.UpdateBiomeGateProperties();
+            showRepeatMessage.SettingChanged += (sender, args) => SE_BiomeGate.UpdateBiomeGateProperties();
+            showStartMessage.SettingChanged += (sender, args) => SE_BiomeGate.UpdateBiomeGateProperties();
+        }
+
+        private static bool IsAdmin()
+        {
+            if (!adminPermitted.Value)
+                return false;
+
+            if (!ZNet.instance)
+                return false;
+
+            return ZNet.instance.LocalPlayerIsAdminOrHost();
+        }
+
+        ConfigEntry<T> config<T>(string group, string name, T defaultValue, ConfigDescription description, bool synchronizedSetting = true)
+        {
+            ConfigEntry<T> configEntry = Config.Bind(group, name, defaultValue, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = configSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        ConfigEntry<T> config<T>(string group, string name, T defaultValue, string description, bool synchronizedSetting = true) => config(group, name, defaultValue, new ConfigDescription(description), synchronizedSetting);
+
+        public void FixedUpdate()
+        {
+            if (Player.m_localPlayer == null)
+                return;
+
+            if (Player.m_localPlayer.GetSEMan().HaveStatusEffect(SE_BiomeGate.statusEffectBiomeGateHash))
+            {
+                if (gatedBiomes.Value == Heightmap.Biome.None || Player.m_localPlayer.m_currentBiome == Heightmap.Biome.None)
+                    Player.m_localPlayer.GetSEMan().RemoveStatusEffect(SE_BiomeGate.statusEffectBiomeGateHash);
+                else if (!gatedBiomes.Value.HasFlag(Player.m_localPlayer.m_currentBiome))
+                    Player.m_localPlayer.GetSEMan().RemoveStatusEffect(SE_BiomeGate.statusEffectBiomeGateHash);
+                else if (IsAdmin())
+                    Player.m_localPlayer.GetSEMan().RemoveStatusEffect(SE_BiomeGate.statusEffectBiomeGateHash);
+            }
+            else
+            {
+                if (gatedBiomes.Value.HasFlag(Player.m_localPlayer.m_currentBiome) && !IsAdmin())
+                    Player.m_localPlayer.GetSEMan().AddStatusEffect(SE_BiomeGate.statusEffectBiomeGateHash);
+            }
+            
+        }
+
+        [HarmonyPatch]
+        public static class PreventInteractions
+        {
+            static IEnumerable<MethodBase> TargetMethods()
+            {
+                return AppDomain.CurrentDomain.GetAssemblies().Where(x => x.FullName.StartsWith("assembly_valheim"))
+                                                                .SelectMany(s => s.GetTypes())
+                                                                .Where(p => typeof(Interactable).IsAssignableFrom(p))
+                                                                .Where(p => p.Name != "Interactable")
+                                                                .SelectMany(t => new List<MethodBase>() { AccessTools.Method(t, "Interact"), AccessTools.Method(t, "UseItem") });
+            }
+
+            private static bool Prefix(object[] __args)
+            {
+                Humanoid user = __args.FirstOrDefault(arg => arg is Humanoid) as Humanoid;
+                if (user != null && user.GetSEMan().HaveStatusEffect(SE_BiomeGate.statusEffectBiomeGateHash))
+                {
+                    user.Message(MessageHud.MessageType.Center, "$msg_nobuildzone");
+                    return false;
+                }
+
+                return true;
+            }
+        }
+
+        [HarmonyPatch(typeof(Player), nameof(Player.SetPlaceMode))]
+        public static class Player_SetPlaceMode_PreventBuilding
+        {
+            private static void Prefix(Player __instance, ref PieceTable buildPieces, ref PieceTable __state)
+            {
+                if (!__instance.GetSEMan().HaveStatusEffect(SE_BiomeGate.statusEffectBiomeGateHash))
+                    return;
+
+                __state = buildPieces;
+                buildPieces = null;
+            }
+
+            private static void Postfix(ref PieceTable buildPieces, ref PieceTable __state)
+            {
+                if (__state == null)
+                    return;
+
+                buildPieces = __state;
+            }
+        }
+
+        [HarmonyPatch(typeof(Player), nameof(Player.InPlaceMode))]
+        public static class Player_InPlaceMode_PreventBuilding
+        {
+            private static bool Prefix(Player __instance, ref bool __result)
+            {
+                if (!__instance.GetSEMan().HaveStatusEffect(SE_BiomeGate.statusEffectBiomeGateHash))
+                    return true;
+
+                __result = false;
+                return false;
+            }
+        }
+        
+        [HarmonyPatch(typeof(Minimap), nameof(Minimap.UpdateExplore))]
+        public static class Minimap_UpdateExplore_PreventExplore
+        {
+            private static void Prefix(Minimap __instance, Player player)
+            {
+                if (!player.GetSEMan().HaveStatusEffect(SE_BiomeGate.statusEffectBiomeGateHash))
+                    return;
+
+                __instance.m_exploreTimer = 0;
+            }
+        }
+    }
+}
---
+++ SE_BiomeGate.cs
@@ -0,0 +1,98 @@
+﻿using HarmonyLib;
+using System;
+using System.Linq;
+using UnityEngine;
+using static HitData;
+using static BiomeGate.BiomeGate;
+
+namespace BiomeGate
+{
+    public class SE_BiomeGate : SE_Stats
+    {
+        public const string statusEffectBiomeGateName = "BiomeGate";
+        public static readonly int statusEffectBiomeGateHash = statusEffectBiomeGateName.GetStableHashCode();
+
+        public const string statusEffectName = "$menu_server_warning";
+        public const string statusEffectTooltip = "$tutorial_blackforest_topic";
+
+        public override string GetIconText()
+        {
+            return Localization.instance.Localize(statusEffectTooltip);
+        }
+
+        public static void UpdateBiomeGateProperties()
+        {
+            if (ObjectDB.instance)
+                UpdateStatusEffectProperties(ObjectDB.instance.GetStatusEffect(statusEffectBiomeGateHash) as SE_BiomeGate);
+
+            if (Player.m_localPlayer != null && Player.m_localPlayer.GetSEMan().HaveStatusEffect(statusEffectBiomeGateHash))
+                UpdateStatusEffectProperties(Player.m_localPlayer.GetSEMan().GetStatusEffect(statusEffectBiomeGateHash) as SE_BiomeGate);
+        }
+
+        private static void UpdateStatusEffectProperties(SE_BiomeGate statusEffect)
+        {
+            if (statusEffect == null)
+                return;
+
+            statusEffect.m_noiseModifier = noiseModifier.Value;
+            statusEffect.m_stealthModifier = sneakModifier.Value;
+            statusEffect.m_damageModifier = damageDoneModifier.Value;
+            statusEffect.m_raiseSkillModifier = raiseSkillModifier.Value;
+
+            statusEffect.m_repeatMessage = showRepeatMessage.Value ? "$npc_dvergrrogue_random_private_area_alarm8" : "";
+            statusEffect.m_startMessage = showStartMessage.Value ? "$npc_dvergr_ashlands_random_private_area_alarm5" : "";
+
+            statusEffect.m_mods.Clear();
+            foreach (DamageType damageType in Enum.GetValues(typeof(DamageType)))
+                if (damageType != DamageType.Damage && damageType != DamageType.Physical && damageType != DamageType.Elemental)
+                    statusEffect.m_mods.Add(new DamageModPair() { m_modifier = damageReceivedModifier.Value, m_type = damageType });
+        }
+
+        [HarmonyPatch(typeof(ObjectDB), nameof(ObjectDB.Awake))]
+        public static class ObjectDB_Awake_AddStatusEffects
+        {
+            public static void AddCustomStatusEffects(ObjectDB odb)
+            {
+                if (odb.m_StatusEffects.Count > 0)
+                {
+                    if (!odb.m_StatusEffects.Any(se => se.name == statusEffectBiomeGateName))
+                    {
+                        SE_BiomeGate statusEffect = ScriptableObject.CreateInstance<SE_BiomeGate>();
+                        statusEffect.name = statusEffectBiomeGateName;
+                        statusEffect.m_nameHash = statusEffectBiomeGateHash;
+                        statusEffect.m_icon = odb.m_StatusEffects.Find(se => se.m_icon != null && se.m_icon.name.IndexOf("immobilized") != -1)?.m_icon;
+                        statusEffect.m_flashIcon = true;
+
+                        statusEffect.m_modifyAttackSkill = Skills.SkillType.All;
+                        statusEffect.m_raiseSkill = Skills.SkillType.All;
+
+                        statusEffect.m_startMessageType = MessageHud.MessageType.Center;
+                        statusEffect.m_repeatMessageType = MessageHud.MessageType.Center;
+                        statusEffect.m_repeatInterval = 20f;
+
+                        statusEffect.m_name = statusEffectName;
+                        statusEffect.m_tooltip = statusEffectTooltip;
+
+                        UpdateStatusEffectProperties(statusEffect);
+
+                        odb.m_StatusEffects.Add(statusEffect);
+                    }
+                }
+            }
+
+            private static void Postfix(ObjectDB __instance)
+            {
+                AddCustomStatusEffects(__instance);
+            }
+        }
+
+        [HarmonyPatch(typeof(ObjectDB), nameof(ObjectDB.CopyOtherDB))]
+        public static class ObjectDB_CopyOtherDB_AddStatusEffects
+        {
+            private static void Postfix(ObjectDB __instance)
+            {
+                ObjectDB_Awake_AddStatusEffects.AddCustomStatusEffects(__instance);
+            }
+        }
+    }
+}
